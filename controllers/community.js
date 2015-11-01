3bae6cb825e3fc96eac9a173206feb205715662a
Christian Carlson <chris.carlson.dev@gmail.com>
2015-11-01 13:21:35 -0800

    added styling for login,sign-up pages

---
+++ controllers/community.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../models');
+var request = require('request');
+
+router.route('/community')
+
+module.exports = router;
