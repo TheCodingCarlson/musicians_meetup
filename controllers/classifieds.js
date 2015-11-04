f22526adb674f5f900b8a3a1a7b343ce73e63212
Christian Carlson <chris.carlson.dev@gmail.com>
2015-11-04 13:39:02 -0800

    made searching more efficient

--- controllers/classifieds.js
+++ controllers/classifieds.js
@@ -5,25 +5,29 @@ var request = require('request');
 
 router.route('/classifieds')
 	.get(function(req, res) {
-		var query = req.query.search;
-		if(query) {
-			db.post.findAll({
-				where: {
-					title: {
-						$iLike: '%'+query+'%'
-					}
-				},
-				include: [db.user]
-			}).then(function(filtered) {
-				res.render('classifieds', {posts: filtered});
-			});
+		if(req.currentUser) {
+			var query = req.query.search;
+			if(query) {
+				db.post.findAll({
+					where: {
+						$or: [
+						{title: {$iLike: '%'+query+'%'}},
+						{body: {$iLike: '%'+query+'%'}}]
+					},
+					include: [db.user]
+				}).then(function(filtered) {
+					res.render('classifieds', {posts: filtered});
+				});
 
+			} else {
+				db.post.findAll({
+					include: [db.user]
+				}).then(function(posts) {
+					res.render('classifieds', {posts: posts});
+				});
+			}
 		} else {
-			db.post.findAll({
-				include: [db.user]
-			}).then(function(posts) {
-				res.render('classifieds', {posts: posts});
-			});
+			res.redirect('/');
 		}
 	});
 
