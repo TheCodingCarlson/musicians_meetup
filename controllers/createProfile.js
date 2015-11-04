8ffc8b097bff38eea6beb20e268349022988d851
Christian Carlson <chris.carlson.dev@gmail.com>
2015-11-03 18:16:36 -0800

    added usability

--- controllers/createProfile.js
+++ controllers/createProfile.js
@@ -2,6 +2,15 @@ var express = require('express');
 var router = express.Router();
 var db = require('../models');
 var request = require('request');
+var multer = require('multer');
+var upload = multer({ dest: './uploads/'});
+var cloudinary = require('cloudinary');
+
+cloudinary.config({ 
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 router.route('/create_profile')
 	.get(function(req, res) {
@@ -15,25 +24,29 @@ router.route('/create_profile')
 		var genres = req.body.genres;
 		var bio = req.body.bio;
 		var lookingFor = req.body.lookingFor;
+		var image = req.body.something;
 
 		db.user.find({
 			where: {
 				email: req.currentUser.email
 			}
-		}).then(function(user) {
-			user.updateAttributes({
-				firstName: firstName,
-				lastName: lastName,
-				instruments: instruments,
-				location: location,
-				genres: genres,
-				bio: bio,
-				lookingFor: lookingFor
-			}).then(function() {
-				req.session.user = user.id;
-				res.redirect("/user_profile");
+	}).then(function(user) {
+		user.updateAttributes({
+			firstName: firstName,
+			lastName: lastName,
+			instruments: instruments,
+			location: location,
+			img: image,
+			genres: genres,
+			bio: bio,
+			lookingFor: lookingFor
+			})
+			.then(function() {
+					req.session.user = user.id;
+					console.log(user.img);
+					res.redirect("/user_profile");
 			});
 		});
-	});
-		
+});
+	
 module.exports = router;
