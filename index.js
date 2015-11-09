440a160b61b2dd4f985b7c3fa8cd00e8f1628e5f
Christian Carlson <chris.carlson.dev@gmail.com>
2015-11-08 18:09:51 -0800

    added links to about page

--- index.js
+++ index.js
@@ -76,18 +76,26 @@ app.get('/user_profile', function(req, res) {
 });
 
 app.get('/user_profile/:id', function(req, res) {
-var id = req.params.id;
-	db.user.findById(id).then(function(user) {
-		var imgUrl = cloudinary.url(user.img, { width: 600, height: 400, crop: "thumb" });
-		user.getFriend().then(function(friend) {
-			var data = {
-				user: user, 
-				friends:friend,
-				userImg: imgUrl
-			};
-			res.render('user_profile', data);
-		});	
-	});
+	var id = req.params.id;
+	if(isNaN(id) === false) {
+		db.user.findById(id).then(function(user) {
+			if(user) {
+				var imgUrl = cloudinary.url(user.img, { width: 600, height: 400, crop: "thumb" });
+				user.getFriend().then(function(friend) {
+					var data = {
+						user: user, 
+						friends:friend,
+						userImg: imgUrl
+					};
+					res.render('user_profile', data);
+				});
+			} else {
+				res.render('error');
+			}
+		});
+	} else { 
+		res.render('error');
+	}
 });
 
 app.delete('/user_profile/:id',function(req, res) {
